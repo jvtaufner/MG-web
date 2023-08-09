eb0a461dcb90a1197b04e2116a556b1b9ebf7914
Taufner <jvtaufner@hotmail.com>
2023-08-09 16:44:24 -0300

    feature(components): create navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import logo from "../../../public/logo.svg";
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-white border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+        <a href="#" className="flex items-center">
+          <Image src={logo} className="h-8 mr-3" alt="Meu Guru Logo" />
+        </a>
+      </div>
+    </nav>
+  );
+};
