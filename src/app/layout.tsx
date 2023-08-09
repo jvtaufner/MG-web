02edcf10e6b05e7904ecfe4c39b65f297c33c291
Taufner <jvtaufner@hotmail.com>
2023-08-09 18:31:27 -0300

    feature: add navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from "react";
+import "./global.css";
+import { Navbar } from "./components/Navbar";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <div className="max-w-screen-xl mx-auto mt-12">{children}</div>
+      </body>
     </html>
   );
 }
