30763999daedbdb2e1104bfd1d45916e27917ffd
Taufner <jvtaufner@hotmail.com>
2023-08-09 17:07:54 -0300

    style(components): increase navbar width

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200">
-      <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+      <div className="flex flex-wrap items-center mx-auto p-4">
         <a href="#" className="flex items-center">
           <Image src={logo} className="h-8 mr-3" alt="Meu Guru Logo" />
         </a>
